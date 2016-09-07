d375a51eeaf9342bac1f0812be00da34507d4952
Rishikesh Gajam <rishi.gajam@gmail.com>
2016-09-06 21:35:43 -0700

    Setup material 2.0.0-alpha.7-9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdCoreModule } from '@angular2-material/core';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdInputModule } from '@angular2-material/input';
+import { MdIconModule } from '@angular2-material/icon';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +19,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdCoreModule,
+    MdToolbarModule,
+    MdCardModule,
+    MdButtonModule,
+    MdInputModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
